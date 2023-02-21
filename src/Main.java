dc89e46e1fc9effd4c725b7700f14f9fb71d0554
Matthew Greco <mgreco2112@tutanota.com>
2023-02-21 10:26:41 -0500

    initial commit, created first Frame, learning methods

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame demoFrame = new Frame("Demo Frame");
+
+        demoFrame.setVisible(true);
+    }
+}
