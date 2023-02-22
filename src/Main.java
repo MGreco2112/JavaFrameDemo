0425f0881b13aea6b35f0c2204b13de73454314f
Matthew Greco <mgreco2112@tutanota.com>
2023-02-22 12:44:47 -0500

    attempted to realize player with MPEG video stored locally, did not function as expected

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import jmapps.ui.JMFrame;
 
 import java.awt.*;
+import java.io.IOException;
 import javax.media.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, NoPlayerException {
         Frame demoFrame = new Frame("Demo Frame");
+
+        //JVM not functioning as expected. Looking into other APIs
+        Player demoPlayer = Manager.createPlayer(new MediaLocator("C:\\Users\\Owner\\Desktop\\sams_song.mpeg"));
 
         demoFrame.setVisible(true);
     }
